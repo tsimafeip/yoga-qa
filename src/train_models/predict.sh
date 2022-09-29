8cdc77328c517052ce18dc2f2a9dc179fc5fcece
tsimafeip <tsimafei.prakapenka@gmail.com>
2022-09-29 11:14:27 +0200

    Update hint for prediction script.

--- src/train_models/predict.sh
+++ src/train_models/predict.sh
@@ -2,5 +2,5 @@ model_path=$1
 test_jsonl=$2
 output=$3
 
-# bash predict.sh archive/mt5_0.0001_batch16_accum8_seed424242/model.tar.gz data/yoga_test.jsonl yoga_test_predictions.txt
+# bash predict.sh archive/mt5_0.0001_batch16_accum8_seed424242/model.tar.gz data/question_to_answer/yoga_test.jsonl predictions/question_to_answer/full_yoga_test_predictions.txtg
 allennlp predict "$model_path" "$test_jsonl" --predictor yoga_predictor --output-file "$output"
